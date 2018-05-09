268058069123e85ce38ac667ede966accf8b0350
Julien Muggli <liozon@hotmail.com>
2018-05-09 09:16:17 +0200

    Updated Item.js

--- ProgInt/src/Models/Master.js
+++ ProgInt/src/Models/Master.js
@@ -2,7 +2,7 @@ export default Backbone.Model.extend({
     initialize: function (attrs, options) {
         this.on("change", function () {
             console.log(this.toJSON());
-            $.post("truc", this.toJSON());
+            //$.post("truc", this.toJSON());
         }) 
     }   
 });
--- ProgInt/src/index.js
+++ ProgInt/src/index.js
@@ -14,11 +14,18 @@ var m2 = new ModelItem({
 //m2.set({title: "truc3"});
 //m2.set({title: "truc5"});
 //m2.set({title: "super truc6789"});
-//
+
 //m1.set({title: "truc"});
 //m1.set({title: "truc2"});
 //m1.set({title: "super truc"});
 
 // 2e cours
 
-var v1 = new ViewItem({model: m1})
+var v1 = new ViewItem({model: m1});
+$(function () { //Attendre que le DOM soit prêt
+    var dom = v1.render();
+    dom.appendTo("body");
+})
+
+v1.render();
+m1.set({title: "nouveau titre!"});
