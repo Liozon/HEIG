09c7636a95945da7574bc00a659840a507382561
Julien Muggli <liozon@hotmail.com>
2018-05-15 15:30:43 +0200

    Working on TP3

--- ProgInt/src/Views/Items.js
+++ ProgInt/src/Views/Items.js
@@ -2,6 +2,26 @@ import tmplDefault from "Templates/Items.html";
 import "Styles/Item.css";
 
 export default Backbone.View.extend({
+    
+    events: {
+        "click .btn_delete": "act_infoUser",
+        "click .btn_add": "act_addTitle"
+    },
+    act_infoUser: function (evt) {
+        console.log("btn delete");
+        let ind = $(evt.target).attr("data-index");
+        let model = this.collection.at(ind);
+        model.destroy();
+        
+    },
+    
+    act_addTitle: function (evt) {
+        console.log("btn add");
+        
+        let ind = $(evt.target).attr("data-index");
+        let model = this.collection.at(ind);
+        
+    },
 
   initialize: function(attrs, options) {
     this.template =  attrs.template || tmplDefault;
