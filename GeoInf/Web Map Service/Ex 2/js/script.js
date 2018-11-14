7cb1c5d8b3fa5380f615a195bf7f2af20d87c037
Julien Muggli <liozon@hotmail.com>
2018-11-14 15:42:14 +0100

    Ex 1 done

--- GeoInf/Web Map Service/Ex 2/js/script.js
+++ GeoInf/Web Map Service/Ex 2/js/script.js
@@ -1,26 +1,69 @@
 var map;
-            $(document).ready(function () {
-                map = new ol.Map({
-                    target: 'map'
-                });
-                var wmsLayer = new ol.layer.Image({
-                    source: new ol.source.ImageWMS({
-                        url: blWMS,
-                        ratio: 1,
-                        params: {
-                            VERSION: "1.0.0",
-                            LAYERS: "ne_10m_admin_0_countries",
-                            FORMAT: "image/png"
-                        }
-                    })
-                });
-                map.addLayer(wmsLayer);
+$(document).ready(function () {
+    map = new ol.Map({
+        target: 'map'
+    });
+    var wmsLayer = new ol.layer.Image({
+        source: new ol.source.ImageWMS({
+            url: blWMS,
+            ratio: 1,
+            params: {
+                VERSION: "1.0.0",
+                LAYERS: "ne_10m_admin_0_countries",
+                FORMAT: "image/png"
+            }
+        })
+    });
+    map.addLayer(wmsLayer);
 
-                // Configuration of the map view
-                var v0 = new ol.View({projection: "EPSG:4326"});
-                var extent_init = [-18, 37, 38, 57]; // or v0.getProjection().getExtent();
-                v0.fit(extent_init, map.getSize());
-                map.setView(v0);  
-                             
-                //showExtent(extent_init);
-            });
+    var myLayer = new ol.layer.Image({
+        source: new ol.source.ImageWMS({
+            url: chWMS,
+            ratio: 1,
+            params: {
+                VERSION: "1.0.0",
+                LAYERS: "ch.swisstopo.swissboundaries3d-kanton-flaeche.fill",
+                FORMAT: "image/png"
+            }
+        })
+    });
+
+    map.addLayer(myLayer);
+
+    var canton = new ol.layer.Image({
+        source: new ol.source.ImageWMS({
+            url: chWMS,
+            ratio: 1,
+            params: {
+                VERSION: "1.0.0",
+                LAYERS: "ch.swisstopo.swissboundaries3d-gemeinde-flaeche.fill",
+                FORMAT: "image/png"
+            }
+        })
+    });
+
+    map.addLayer(canton);
+
+    var myLayer2 = new ol.layer.Image({
+        source: new ol.source.ImageWMS({
+            url: chWMS,
+            ratio: 1,
+            params: {
+                VERSION: "1.0.0",
+                LAYERS: "ch.bazl.sicherheitszonenplan",
+                FORMAT: "image/png"
+            }
+        })
+    });
+
+    map.addLayer(myLayer2);
+
+    // Configuration of the map view
+    var v2 = new ol.View({ projection: "EPSG:4326" });
+    var cbox = [10, 47]; // NB: c'est le centre de l'extent_init ci-dessus
+    v2.setCenter(cbox);
+    v2.setZoom(5);
+    map.setView(v2);
+
+    //showExtent(extent_init);
+});
